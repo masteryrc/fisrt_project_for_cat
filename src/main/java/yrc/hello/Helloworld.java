b7c3ff47b43693962bf90a20d805f9eeb5fcd6c1
masteryrc <1045056459@qq.com>
2022-04-20 21:59:57 +0800

    add static method

--- src/main/java/yrc/hello/Helloworld.java
+++ src/main/java/yrc/hello/Helloworld.java
@@ -2,12 +2,17 @@ package yrc.hello;
 
 public class Helloworld {
     private String string_hello_world = "hello world!";
+    private static String string_hello_world_static = "hello world! (static)";
     public void print_hello_world(){
         System.out.println(string_hello_world);
+    }
+    public static void  print_hello_world_static(){
+        System.out.println(string_hello_world_static);
     }
     public static void main(String[] args){
         Helloworld helloworld = new Helloworld();
         helloworld.print_hello_world();
+        Helloworld.print_hello_world_static();
     }
 
 }
